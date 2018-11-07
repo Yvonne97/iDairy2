b182982b7a26919fdcfa3dac42b06010ea6cbd3e
Yvonne97 <31758496+Yvonne97@users.noreply.github.com>
2018-11-07 19:12:18 +0300

    Update AddDisease.java

--- app/src/main/java/com/example/hp/milkproject/AddDisease.java
+++ app/src/main/java/com/example/hp/milkproject/AddDisease.java
@@ -1,34 +1,52 @@
 package com.example.hp.milkproject;
 
+import android.app.ProgressDialog;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
-public class AddDisease extends AppCompatActivity {
+public class AddDisease extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
     EditText editTextDis, editTextDesc;
     Button buttonDone;
+    Spinner spinner;
+    ArrayList<String> stringsCategoryName;
+    String cowName;
+    ProgressDialog progressDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_disease);
 
+        progressDialog = new ProgressDialog(this);
+
         editTextDis = (EditText) findViewById(R.id.editTextNameDis);
         editTextDesc = (EditText) findViewById(R.id.editTextDesc);
+
+        spinner = (Spinner) findViewById(R.id.spinner);
+
+        spinner.setOnItemSelectedListener(this);
 
         buttonDone = (Button) findViewById(R.id.buttonDone);
 
@@ -55,6 +73,68 @@ public class AddDisease extends AppCompatActivity {
             }
         });
 
+        getCategoryNames();
+
+
+    }
+
+    private void getCategoryNames() {
+
+        progressDialog.setMessage("Loading...");
+        progressDialog.setCancelable(false);
+        progressDialog.show();
+
+        if (stringsCategoryName != null) {
+            stringsCategoryName.clear();
+        }
+
+        stringsCategoryName = new ArrayList<String>();
+        stringsCategoryName.add("Choose Cow");
+
+        DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference();
+        databaseReference.child("Cows").addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                for (DataSnapshot postSnapshot : dataSnapshot.getChildren()) {
+                    AddMilking.GetName getName = postSnapshot.getValue(AddMilking.GetName.class);
+                    stringsCategoryName.add(getName.getName());
+                }
+                final ArrayAdapter<String> adapterCategory = new ArrayAdapter<>(AddDisease.this, android.R.layout.simple_spinner_item, stringsCategoryName);
+                adapterCategory.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                spinner.setAdapter(adapterCategory);
+                progressDialog.dismiss();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+    }
+
+    public static class GetName {
+        String name;
+
+        public GetName() {
+        }
+
+        public GetName(String name) {
+            this.name = name;
+        }
+
+        public String getName() {
+            return name;
+        }
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        cowName = parent.getItemAtPosition(position).toString();
+
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
 
     }
 }
